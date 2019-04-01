7a9ceecdffe6f9a2a7c157006c7f3a8053f6d284
d-Lijiawei <824918825@qq.com>
2019-04-01 21:25:26 +0800

    登录

--- application/shop/controller/MobileBase.php
+++ application/shop/controller/MobileBase.php
@@ -55,9 +55,9 @@ class MobileBase extends Controller {
                 if(is_array($this->weixin_config) && $this->weixin_config['wait_access'] == 1){
                     $wxuser = $this->GetOpenid(); //授权获取openid以及微信用户信息
                  
-                        $old_openid = $this->GetOldOpenid(); //授权获取openid以及微信用户信息
-                        $wxuser['old_openid'] = $old_openid['old_openid'];
-                        dump($wxuser);exit;
+                    $old_openid = $this->GetOldOpenid(); //授权获取openid以及微信用户信息
+                    $wxuser['old_openid'] = $old_openid['old_openid'];
+
                     //过滤特殊字符串
                     $wxuser['nickname'] && $wxuser['nickname'] = replaceSpecialStr($wxuser['nickname']);
                     
