f79c82746aab782f3eb37dcb7a75169872ba3950
liutianiang <360033789@qq.com>
2019-04-16 18:27:37 +0800

    ceshi

--- application/shop/controller/Cart.php
+++ application/shop/controller/Cart.php
@@ -325,12 +325,12 @@ class Cart extends MobileBase {
             {
                 $bankCodeList[$val['code']] = unserialize($val['bank_code']);
             }
-            
+            /*
             if($key != 'cod' && (($key == 'weixin' && !is_weixin()) // 不是微信app,就不能微信付，只能weixinH5付,用于手机浏览器
                 || ($key != 'weixin' && is_weixin()) //微信app上浏览，只能微信
-                /*|| ($key != 'alipayMobile' && is_alipay())*/)){ //在支付宝APP上浏览，只能用支付宝支付
+                || ($key != 'alipayMobile' && is_alipay()))){ //在支付宝APP上浏览，只能用支付宝支付
                 unset($paymentList[$key]);
-            }
+            }*/
         }
         $bank_img = include APP_PATH.'home/bank.php'; // 银行对应图片
         $this->assign('paymentList',$paymentList);
