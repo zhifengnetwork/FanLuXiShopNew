f422c7e617c8a575548012acd7f3b2961110808e
FZJ <yudwcom@163.com>
2019-04-20 14:52:03 +0800

    解决 会员 详情报错

--- application/admin/controller/User.php
+++ application/admin/controller/User.php
@@ -189,9 +189,7 @@ class User extends Base
          }
  
          $user['first_lower'] = M('users')->where("first_leader = {$user['user_id']}")->count();
-         // $user['second_lower'] = M('users')->where("second_leader = {$user['user_id']}")->count();
-         // $user['third_lower'] = M('users')->where("third_leader = {$user['user_id']}")->count();
- 
+       
          $this->assign('user', $user);
          return $this->fetch();
      }
