abae0404f4835c4ec95072b7d3ec4eb69c79e41f
FZJ <yudwcom@163.com>
2019-04-20 21:16:10 +0800

    生成二维码报错

--- application/common/logic/ShareLogic.php
+++ application/common/logic/ShareLogic.php
@@ -58,6 +58,20 @@ class ShareLogic
 
     
     public function getImage($url,$save_dir='',$filename='',$type=0){
+
+        //本地图片
+        if(strpos($url,'public') !== false){ 
+            $url = ROOT_PATH.$url;
+        }else{
+            //是微信图片
+            $end = substr($url,-3);
+            if($end == '132'){
+                $url = substr($url,0,count($url)-4).'0';
+            }
+
+        }
+
+
         if(trim($url)==''){
             return array('file_name'=>'','save_path'=>'','error'=>1);
         }
