abe1ec08350c60281b539d5e1133f90499682d4e
杨宇材 <13265947403@163.com>
2019-04-24 22:53:15 +0800

    支付宝提现

--- application/admin/controller/AlipayTransfer.php
+++ application/admin/controller/AlipayTransfer.php
@@ -59,6 +59,7 @@ class AlipayTransfer extends Base
             "\"remark\":\"$remark\"" .
             "}");
         $result = $aop->execute($request);
+        dump($result);
         $responseNode = str_replace(".", "_", $request->getApiMethodName()) . "_response";
         $resultCode = $result->$responseNode->code;
         if(!empty($resultCode)&&$resultCode == 10000){
