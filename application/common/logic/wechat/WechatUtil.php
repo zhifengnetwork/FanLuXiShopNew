c7eafdd347af370fe053c1b4fed466c2591932da
liutianiang <360033789@qq.com>
2019-04-24 16:27:21 +0800

    ceshi

--- application/common/logic/wechat/WechatUtil.php
+++ application/common/logic/wechat/WechatUtil.php
@@ -1062,7 +1062,7 @@ class WechatUtil extends WxCommon
 
         
         if($msg=='1' || $msg[$key] == 'LOCATION') return;
-
+        /*
          $openid =$msg['FromUserName'];
           $first_leader = $msg['EventKey'];
          if(!empty($first_leader))
@@ -1089,7 +1089,7 @@ class WechatUtil extends WxCommon
              // }
               
           
-         }
+         }*/
 
         //Db::name('users')->where('user_id=17421400')->update(['first_leader'=>1235]);
         $return= $this->createReplyMsgOfText($msg['ToUserName'], $msg['FromUserName'], '你已关注公众号');
