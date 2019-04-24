120b54464c526f4234ba1a113ee250503ad84496
杨宇材 <13265947403@163.com>
2019-04-24 23:25:50 +0800

    支付宝提现

--- application/admin/controller/AlipayTransfer.php
+++ application/admin/controller/AlipayTransfer.php
@@ -39,6 +39,7 @@ class AlipayTransfer extends Base
 
     public function pay ($out_biz_no,$payee_account,$amount,$payer_show_name,$payee_real_name,$remark) {
         vendor('alipaysdk.AopSdk');
+        echo 111133;die;
         $aop = new \AopClient ();
         echo 1111;die;
         $aop->appId = $this->appId;
