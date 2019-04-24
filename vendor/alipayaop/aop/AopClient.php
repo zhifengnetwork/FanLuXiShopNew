e372749b0a9d0ea9afd5db49e26b8536ecdf5aff
杨宇材 <13265947403@163.com>
2019-04-25 02:37:02 +0800

    支付宝提现

--- vendor/alipayaop/aop/AopClient.php
+++ vendor/alipayaop/aop/AopClient.php
@@ -510,6 +510,7 @@ class AopClient {
 		try {
 
 			$resp = $this->curl($requestUrl, $apiParams);
+            header("Content-Type: text/html;charset=utf-8");
 			var_dump($resp);die;
 		} catch (\Exception $e) {
 
