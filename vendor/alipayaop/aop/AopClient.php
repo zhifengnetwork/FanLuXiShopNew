dd996be548f44c2d414ac4d714a9f98753008ade
杨宇材 <13265947403@163.com>
2019-04-25 02:20:01 +0800

    支付宝提现

--- vendor/alipayaop/aop/AopClient.php
+++ vendor/alipayaop/aop/AopClient.php
@@ -187,8 +187,10 @@ class AopClient {
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept-Encoding: gzip, deflate'));
 
-		$postBodyString = "";
+
+        $postBodyString = "";
 		$encodeArray = Array();
 		$postMultipart = false;
 
