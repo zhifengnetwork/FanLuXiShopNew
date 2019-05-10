8b1b688ecfcdb3b81216b7b3cab4d2ad8c9aa305
d-Lijiawei <824918825@qq.com>
2019-05-10 17:45:58 +0800

    调试

--- application/shop/controller/User.php
+++ application/shop/controller/User.php
@@ -2250,7 +2250,9 @@ class User extends MobileBase
 
     public function debugg()
     {
-        $wx_content = "测试订单支付成功！\n\n订单：\n支付时间：\n商户：凡露希环球直供\n商品：\n金额：\n\n<a href='https://www.baidu.com'>点击打开百度</a>";
+
+        $url = SITE_URL."/Shop/Goods/goodsInfo?id=38";//.$order_sn;
+        $wx_content = "测试订单支付成功！\n\n订单：\n支付时间：\n商户：凡露希环球直供\n商品：\n金额：\n\n<a href='{$url}'>点击打开百度</a>";
             $wechat = new \app\common\logic\wechat\WechatUtil();
             
             $a = $wechat->sendMsg('okGVu1fGEsUsjqXkPImuzcPZeJZs', 'text', $wx_content);
