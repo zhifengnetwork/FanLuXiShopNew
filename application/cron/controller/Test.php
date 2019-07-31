91476d42f15836b47c287136003db03b77ec057b
FZJ <yudwcom@163.com>
2019-08-01 02:58:10 +0800

    pao

--- application/cron/controller/Test.php
+++ application/cron/controller/Test.php
@@ -27,49 +27,53 @@ class Test extends Controller{
 
 dump($shang);
 
+
             $order_amount = $vv['money'];
 
-            foreach ($shang as $k => $v) {
-
-
-                $cunzai = M('agent_performance')->where(['user_id'=>$v['user_id']])->find();
-                //存在
-                if ($cunzai) {
-                    $data11['team_per'] = $cunzai['team_per'] + $order_amount;
-                    $data11['update_time'] = date('Y-m-d H:i:s');
-                    $res = M('agent_performance')->where(['user_id'=>$v['user_id']])->save($data11);
-                } else {
-                    $data1['user_id'] =  $v['user_id'];
-                    $data1['team_per'] =  $order_amount;
-                    $data1['create_time'] = date('Y-m-d H:i:s');
-                    $data1['update_time'] = date('Y-m-d H:i:s');
-                    $res = M('agent_performance')->add($data1);
+            if ($shang) {
+                foreach ($shang as $k => $v) {
+                    $cunzai = M('agent_performance')->where(['user_id'=>$v['user_id']])->find();
+                    //存在
+                    if ($cunzai) {
+                        $data11['team_per'] = $cunzai['team_per'] + $order_amount;
+                        $data11['update_time'] = date('Y-m-d H:i:s');
+                        $res = M('agent_performance')->where(['user_id'=>$v['user_id']])->save($data11);
+                    } else {
+                        $data1['user_id'] =  $v['user_id'];
+                        $data1['team_per'] =  $order_amount;
+                        $data1['create_time'] = date('Y-m-d H:i:s');
+                        $data1['update_time'] = date('Y-m-d H:i:s');
+                        $res = M('agent_performance')->add($data1);
+                    }
                 }
-
             }
-
 
             //个人业绩
 
             $user_id = $vv['user_id'];
 
+
+            unset($cunzai);
             $cunzai = M('agent_performance')->where(['user_id'=>$user_id])->find();
+
+           
             //存在
             if($cunzai){
+              
                 $data_new['ind_per'] = $cunzai['ind_per'] + $order_amount;
                 $data_new['update_time'] = date('Y-m-d H:i:s');
                 $res = M('agent_performance')->where(['user_id'=>$user_id])->save($data_new);
-    
-                
-    
+
             }else{
     
-                $data['user_id'] =  $user_id;
-                $data['ind_per'] =  $order_amount;
-                $data['create_time'] = date('Y-m-d H:i:s');
-                $data['update_time'] = date('Y-m-d H:i:s');
-                $res = M('agent_performance')->add($data);
-    
+                 
+        
+                $data22['user_id'] =  $user_id;
+                $data22['ind_per'] =  $order_amount;
+                $data22['create_time'] = date('Y-m-d H:i:s');
+                $data22['update_time'] = date('Y-m-d H:i:s');
+                $res = M('agent_performance')->add($data22);
+
             }
 
 
